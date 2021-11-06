a532b77c81859eb9e34fc45a5b825077279d1864
Ubuntu <stefanbabukov98@gmail.com>
2021-11-06 16:14:55 +0000

    update

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,6 +7,11 @@ class Dec2Hex
 
     public static void main(String args[])
     {
+	if(args.length==0){
+		throw new Exception("No number provided!");
+	}
+	System.out.println("number is type" + args[0].getType());
+	
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
